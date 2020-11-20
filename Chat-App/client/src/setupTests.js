ab9ac8611a974261c00d1392babbfb5c754b8f15
Jason Peterson <jroypeterson@gmail.com>
2020-11-20 17:01:59 -0500

    added travis.yml file

--- Chat-App/client/src/setupTests.js
+++ Chat-App/client/src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// react-testing-library renders your components to document.body,
+// this adds jest-dom's custom assertions
+import '@testing-library/jest-dom';
