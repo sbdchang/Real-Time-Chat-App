95f2f56afa56de0ed5c3217b3a25251be0fcb814
jroypeterson <jroypeterson@gmail.com>
2020-12-22 20:09:36 -0500

    testing update

--- Chat-App/client/src/components/Lobby.test.js
+++ Chat-App/client/src/components/Lobby.test.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import Lobby from './Lobby';
-import MainView from './MainView';
+//import MainView from './MainView';
 import renderer from 'react-test-renderer';
 
-
+/*
 
 it('Lobby snapshot', () => {
-  const tree = renderer.create(<MainView />).toJSON();
+  const tree = renderer.create(<Lobby />).toJSON();
   expect(tree).toMatchSnapshot();
 });
-
+*/
 
 // it('renders without crashing', () => {
 //   const div = document.createElement('div');
 //   ReactDOM.render(<Lobby />, div);
 // });
+//are the tests working
+it('Testing to see if Jest works', () => {
+  expect(1).toBe(1);
+});
--- Chat-App/client/src/components/VideoChat.test.js
+++ Chat-App/client/src/components/VideoChat.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render, screen } from '@testing-library/react';
-import UserProfile from './UserProfile';
+//import { render, screen } from '@testing-library/react';
+//import UserProfile from './UserProfile';
 import renderer from 'react-test-renderer';
 import VideoChat from './VideoChat';
-import MainView from './MainView';
+//import MainView from './MainView';
 
 it('Videochat Snapshot  ', () => {
-    const tree = renderer.create(<MainView />).toJSON();
+    const tree = renderer.create(<VideoChat />).toJSON();
     expect(tree).toMatchSnapshot();
 });
 
